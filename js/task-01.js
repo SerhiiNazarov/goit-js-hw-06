1dd9b7a57ca3d536fbf0b1e1ed03d3c90bdd0e38
SerhiiNazarov <algazira89@gmail.com>
2022-09-04 21:47:43 +0300

    create proect

---
+++ js/task-01.js
@@ -0,0 +1,13 @@
+const numberofCategories = document.querySelector("#categories");
+console.log(numberofCategories);
+
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
